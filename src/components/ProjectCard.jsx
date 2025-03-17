66232b001f137c65c912a8668931c3cab78ab701
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-03-17 00:56:31 +0000

    update project card add show site option

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,7 @@
-import { div } from "framer-motion/client";
 import { useState } from "react";
 import { IconContext } from "react-icons";
 
-const ProjectCard = ({ imgURL, label, subtext, languages }) => {
+const ProjectCard = ({ imgURL, label, subtext, languages, url }) => {
   const [isOnHover, setIsOnHover] = useState(null);
 
   const handleHoverOn = (id) => {
@@ -24,38 +23,49 @@ const ProjectCard = ({ imgURL, label, subtext, languages }) => {
       <div className="p-2">
         <h1 className="font-bold mb-5 text-2xl">{label}</h1>
         <hr className="bg-primary mb-2 opacity-30" />
-        <div className="flex flex-col justify-between h-30">
-        <p className="text-sm sm:text-base text-white/70">{subtext}</p>
-        <div className="flex gap-2 ">
-          {languages.map((language) => (
-            <div key={language.id}>
-              <IconContext.Provider
-                value={{
-                  color: "var(--color-primary-100)",
-                  size: "40",
-                  className: "border-1 border-red rounded-full p-2 ",
-                }}
-              >
-                <div
-                  className="flex flex-col-reverse  items-center gap-2 relative"
-                  onMouseEnter={() => {
-                    handleHoverOn(language.id);
-                  }}
-                  onMouseLeave={handleHoverOff}
-                >
-                  {language.icon}
-                  <p
-                    className={`absolute -top-8 whitespace-nowrap text-center bg-primary text-black px-2 rounded-md font-semibold ${
-                      isOnHover === language.id ? "block" : "hidden"
-                    }`}
+        <div className="flex flex-col justify-between h-35">
+          <p className="text-sm sm:text-base text-white/70">{subtext}</p>
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2">
+              {languages.map((language) => (
+                <div key={language.id}>
+                  <IconContext.Provider
+                    value={{
+                      color: "var(--color-primary-100)",
+                      size: "40",
+                      className: "border-1 border-red rounded-full p-2 ",
+                    }}
                   >
-                    {language.name}
-                  </p>
+                    <div
+                      className="flex flex-col-reverse items-center gap-2 relative cursor-pointer"
+                      onMouseEnter={() => {
+                        handleHoverOn(language.id);
+                      }}
+                      onMouseLeave={handleHoverOff}
+                    >
+                      <a href="" className="hover:opacity-50 ">
+                        {language.icon}
+                      </a>
+                      <p
+                        className={`absolute -top-8 whitespace-nowrap text-center bg-primary text-black px-2 rounded-md font-semibold ${
+                          isOnHover === language.id ? "block" : "hidden"
+                        }`}
+                      >
+                        {language.name}
+                      </p>
+                    </div>
+                  </IconContext.Provider>
                 </div>
-              </IconContext.Provider>
+              ))}
             </div>
-          ))}
-        </div>
+            <div>
+              <button className="w-20 p-1 bg-transparent text-primary border-2 border-primary">
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  Show
+                </a>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
