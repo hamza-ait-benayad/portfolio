111a507c299017627cc79c63259bdd10bd2d18d7
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-03-21 02:37:39 +0000

    update navigation for the mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,53 @@
+import { IconContext } from "react-icons";
 import { navLinks } from "../constants/index";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuView = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-    <header className="px-8 sm:px-32 py-4 w-full z-10 fixed top-0 bg-background shadow-2xl shadow-primary/12 text-primary ">
-      <nav className="flex justify-between items-center ">
-        <a className="font-bold text-4xl font-mono  leading-normal ">
-          Portfolio
-        </a>
-        <ul className="flex gap-10 max-lg:hidden">
+    <header className="px-8 sm:px-32 py-4 w-full z-10 fixed -top-2 bg-background shadow-2xl shadow-primary/12 text-primary">
+      <nav
+        className={`flex ${
+          isMenuOpen && "max-lg:flex-col max-lg:h-[100vh]"
+        } gap-20 items-center`}
+      >
+        <div className="flex justify-between w-full">
+          <a className="font-bold text-4xl font-mono leading-normal ">
+            Portfolio
+          </a>
+          <div className="flex gap-10 lg:hidden">
+            <IconContext.Provider
+              value={{
+                color: "var(--color-primary-100)",
+                size: "50",
+                className: "",
+              }}
+            >
+              <div onClick={handleMenuView}>
+                {isMenuOpen ? <IoClose /> : <IoMenu />}
+              </div>
+            </IconContext.Provider>
+          </div>
+        </div>
+        <ul className={`flex gap-10 flex-col lg:flex-row ${isMenuOpen ? "flex" : "max-sm:hidden"}`}>
           {navLinks.map((link) => (
             <li key={link.label}>
               <a
                 href={link.href}
                 className="text-2xl font-mono hover:text-secondy transition-colors font-bold leading-normal"
+                onClick={handleMenuView}
               >
                 {link.label}
               </a>
             </li>
           ))}
         </ul>
-        <div className="flex gap-10 sm:hidden">
-          <h1>==</h1>
-        </div>
       </nav>
     </header>
   );
