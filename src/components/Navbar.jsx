7deb7c4251249f108b46684a3cf69fd0aa1a8031
hamza-ait-benayad <aitbenayad.hamza@gmail.com>
2025-03-21 02:50:01 +0000

    fix some style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,14 @@ function Navbar() {
     setIsMenuOpen(!isMenuOpen);
   };
   return (
-    <header className="px-8 sm:px-32 py-4 w-full z-10 fixed -top-2 bg-background shadow-2xl shadow-primary/12 text-primary">
+    <header className="px-8 sm:px-32 py-4 w-full z-10 fixed top-0 bg-background shadow-2xl shadow-primary/12 text-primary">
       <nav
         className={`flex ${
           isMenuOpen && "max-lg:flex-col max-lg:h-[100vh]"
-        } gap-20 items-center`}
+        } gap-20 items-center `}
       >
         <div className="flex justify-between w-full">
-          <a className="font-bold text-4xl font-mono leading-normal ">
+          <a className="font-bold text-3xl sm:text-4xl font-mono leading-normal ">
             Portfolio
           </a>
           <div className="flex gap-10 lg:hidden">
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -33,7 +33,7 @@ export default function Hero() {
   return (
     <section className="flex gap-12 sm:justify-between sm:flex-row flex-col items-center max-container px-4 sm:px-16 h-full sm:h-[100vh] mt-28 sm:mt-0">
       <motion.div
-        className="text-4xl font-bold flex flex-col gap-7 "
+        className="text-2xl sm:text-6xl font-bold flex flex-col sm:gap-7 gap-4"
         initial="hidden"
         whileInView="visible"
         variants={variants}
@@ -45,7 +45,7 @@ export default function Hero() {
         </motion.h1>
         <motion.h1 variants={variants}>
           <motion.span
-            className="bg-secondy px-2 text-black text-6xl leading-21 xl:leading-none"
+            className="bg-secondy px-2 text-black text-5xl sm:text-6xl leading-18 xl:leading-none"
             variants={variants}
           >
             Web & Front End
@@ -53,7 +53,7 @@ export default function Hero() {
         </motion.h1>
         <motion.h1 variants={variants}>
           <motion.span
-            className="text-secondy px-2 text-5xl"
+            className="text-secondy px-2 text-3xl sm:text-5xl"
             variants={variants}
           >
             Developper
@@ -66,7 +66,7 @@ export default function Hero() {
                 value={{
                   color: "var(--color-primary-100)",
                   size: "50",
-                  className: "border-1 border-red rounded-full p-2 ",
+                  className: "border-1 border-red rounded-full p-2",
                 }}
               >
                 <div className="flex flex-col-reverse items-center gap-2 relative cursor-pointer">
@@ -89,11 +89,11 @@ export default function Hero() {
         whileInView="visible"
         variants={imageVariants}
       >
-        <div className="relative bg-black flex justify-center items-center rounded-xl after:content-[''] p-8 after:absolute after:h-[100%] after:w-[100%] after:rounded-md after:bg-transparent after:border-[32px] after:-z-10 after:border-primary after:blur-2xl after:opacity-30 shadow-ligh">
+        <div className="relative bg-black flex justify-center items-center rounded-xl after:content-[''] p-8 after:absolute after:h-[100%] after:w-[100%] after:rounded-md after:bg-transparent after:border-[32px] after:-z-10 after:border-primary after:blur-2xl after:opacity-40 shadow-ligh">
           <img
             src="/images/undraw_hello_ccwj.svg"
             alt="portfolio-image"
-            className="h-[300px]"
+            className="h-[200px]"
           />
         </div>
       </motion.div>
